9d3fdef57879c1d38fe5fe209a9b1f64b4bac7ea
Niklas <github@niklasforst.de>
2019-01-21 23:45:59 +0100

    UPDATE

--- HastebinAPI.java
+++ HastebinAPI.java
@@ -1,5 +1,3 @@
-package test;
-
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 
@@ -13,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 
 public class HastebinAPI {
 
-    private final JsonParser JSON_PARSER = new JsonParser();
+    private static final JsonParser JSON_PARSER = new JsonParser();
 
     public String upload(String content) {
         String result;
@@ -22,20 +20,20 @@ public class HastebinAPI {
             int postDataLength = postData.length;
             String request = "https://hastebin.com/documents";
             URL url = new URL(request);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setInstanceFollowRedirects(false);
-            conn.setRequestMethod("POST");
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
-            conn.setRequestProperty("Content-Type", "text/plain");
-            conn.setRequestProperty("charset", "utf-8");
-            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
-            conn.setUseCaches(false);
-            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection.setDoOutput(true);
+            httpURLConnection.setInstanceFollowRedirects(false);
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            httpURLConnection.setRequestProperty("Content-Type", "text/plain");
+            httpURLConnection.setRequestProperty("charset", "utf-8");
+            httpURLConnection.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            httpURLConnection.setUseCaches(false);
+            DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
             wr.write(postData);
-            Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            Reader bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream(), "UTF-8"));
 
-            String json = ((BufferedReader) in).readLine();
+            String json = ((BufferedReader) bufferedReader).readLine();
 
             JsonElement jsonElement = JSON_PARSER.parse(json);
             if (!jsonElement.getAsJsonObject().has("key")) {
